16f76beff52b88e61512e46080159e8a89c13bcd
WeirdMax <115849473+WeirdMax@users.noreply.github.com>
2023-12-26 00:14:33 +0200

    Add files via upload

---
+++ lab4/Combination.java
@@ -0,0 +1,16 @@
+
+public class Combination {
+    private String combinationName;
+
+    public Combination(String combinationName) {
+        this.combinationName = combinationName;
+    }
+
+    public String getCombinationName() {
+        return combinationName;
+    }
+
+    public void setCombinationName(String combinationName) {
+        this.combinationName = combinationName;
+    }
+}
