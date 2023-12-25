63f6527e62951e45c933f8af050b407596f36fbd
WeirdMax <115849473+WeirdMax@users.noreply.github.com>
2023-12-25 23:30:52 +0200

    Create Main.java

---
+++ lab1/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        TrapezoidalIntegration trapezoidalIntegration = new TrapezoidalIntegration(1000, 0.001, 0.00001);
+        SimpsonIntegration simpsonIntegration = new SimpsonIntegration(1000, 0.001, 0.00001);
+        Function<Double, Double> expression = x -> x * x;
+        double lowerBound = 0;
+        double upperBound = 1;
+        double trapezoidalResult = trapezoidalIntegration.calc(expression, lowerBound, upperBound);
+        double simpsonResult = simpsonIntegration.calc(expression, lowerBound, upperBound);
+        System.out.println("Trapezoidal integration result: " + trapezoidalResult);
+        System.out.println("Simpson integration result: " + simpsonResult);
+    }
+}
